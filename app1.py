538cd5c2eeda78692d4260b624c6b7df8140c805
Natalia-Ushakova <n.n.ushakova@gmail.com>
2018-12-30 00:15:24 -0500

    Expand function definition(word)

--- app1.py
+++ app1.py
@@ -5,15 +5,26 @@ Created on Fri Dec 28 01:53:50 2018
 @author: lenovo
 """
 import json
+from difflib import get_close_matches #method to campare sequences
+
 data = json.load(open("data.json"))
 
 def definition(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.7)) > 0:
+        yn = input("Did you mean %s instead? Enter Y or N: " %get_close_matches(word, data.keys(), cutoff = 0.7)[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys(), cutoff = 0.7)[0]]
+        elif yn == "N":
+            return "The word does not exist."
+        else:
+            return "We did not undestood your entry."
     else:
-        return "The word is not in  the dictionary"
+        return "The word does not exist. Please double check it."
 
 
 word = input("Enter word: ")
+output = definition(word)
 print(definition(word))
